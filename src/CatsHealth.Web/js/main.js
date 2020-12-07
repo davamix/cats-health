cb482b09b97ce5ed56ab495aecec51da10f60612
Daniel Valcarce <davamix@gmail.com>
2020-12-07 01:19:35 +0100

    Updated index with profile cards and new button

--- src/CatsHealth.Web/js/main.js
+++ src/CatsHealth.Web/js/main.js
@@ -2,13 +2,66 @@ import { getRequestTo, postRequestTo } from "./requests.js";
 
 const URL_PROFILES = "https://localhost:5001/api/Profile";
 
-// Test GET
-getRequestTo(URL_PROFILES).then(data => console.log("GET: ", data));
+const cardsContainer = document.getElementById("profile-cards-container");
+const coverImage = "https://via.placeholder.com/400x225.webp?text=Profile+Picture";
+
+function loadProfiles(){
+    getRequestTo(URL_PROFILES)
+        .then(data => {
+            cardsContainer.innerHTML = "";
+            
+            data.forEach(profile =>{
+                addProfileCard(profile);
+            });
+
+            addProfileNewButton();
+        });
+}
+
+function addProfileCard(profile){
+    const profileEl = document.createElement("div");
+    const profileImage = (profile.image) ? profile.image : coverImage;
+
+    profileEl.classList.add("profile-card");
+
+    profileEl.innerHTML = `
+        <img src="${profileImage}" class="profile-card-picture" title="${profile.name}" />
+        
+        <div class="profile-card-name">
+            <h3>${profile.name}</h3>
+        </div>
+    `
+
+    profileEl.addEventListener("click", () =>{
+        console.log(`Go to ${profile.name} dashboard`);
+    });
+
+    cardsContainer.appendChild(profileEl);
+}
+
+function addProfileNewButton(){
+    const profileEl = document.createElement("div");
+    profileEl.classList.add("profile-card-add");
+
+    profileEl.innerHTML = `
+        <button class="profile-add-btn" title="Add new profile">
+            <i class="fas fa-paw fa-10x"></i>
+        </button>
+    `
+
+    profileEl.addEventListener("click", ()=>{
+        console.log("Add new profile");
+    });
+
+    cardsContainer.appendChild(profileEl);
+}
 
 
 // Test POST
-const profile = {
-    name: "P1"
-};
+// const profile = {
+//     name: "P1"
+// };
 
-postRequestTo(URL_PROFILES, profile).then(data => console.log("POST: ", data));
+// postRequestTo(URL_PROFILES, profile).then(data => console.log("POST: ", data));
+
+loadProfiles();
