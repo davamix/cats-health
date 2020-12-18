3667f11c7be622c8e5c577e20d566d5708f11d08
Daniel Valcarce <davamix@gmail.com>
2020-12-18 05:08:12 +0100

    Added Weight into Profile entity

--- src/CatsHealth.API/Data/Entities/Profile.cs
+++ src/CatsHealth.API/Data/Entities/Profile.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace CatsHealth.API.Data.Entities
 {
     public class Profile : EntityBase<Profile>
     {
         public string Name { get; set; }
+        public string Species { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Sex { get; set; }
+        public Weight LastWeight { get; set; }
     }
 }
